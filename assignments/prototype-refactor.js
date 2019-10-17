71694f0d749d8f77016528196e49836cd5e52ce3
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-10-17 14:34:45 -0700

    aeson-johnson

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -8,36 +8,38 @@ Prototype Refactor
 
 */
 
-function GameObject(attributes){
+class GameObject{
+    constructor(attributes){
     this.createdAt = attributes.createdAt,
     this.name = attributes.name,
     this.dimensions = attributes.dimensions
-  }
-  
-  GameObject.prototype.destroy = function(){
+    }
+    destroy(){
     return `${this.name} was removed from the game.`
-  }
+    }
+}
+
+class CharacterStats extends GameObject{
+    constructor(attributes){
+    super(attributes);
+    this.healthPoints = attributes.healthPoints
+    }
+    takeDamage(){
+        return `${this.name} took damage.`
+    }
+}
   
-function CharacterStats(CSAttributes){
-    GameObject.call(this, CSAttributes),
-    this.healthPoints = CSAttributes.healthPoints
-  }
-  
-CharacterStats.prototype.takeDamage = function(){
-    return `${this.name} took damage.`
-  }
-  
-  function Humanoid(humAttributes){
-    GameObject.call(this, humAttributes),
-    CharacterStats.call(this, humAttributes),
-    this.team = humAttributes.team,
-    this.weapons = humAttributes.weapons,
-    this.language = humAttributes.language
-  }
-  
-  Humanoid.prototype.greet = function(){
-    return `${this.name} offers a greeting in ${this.language}.`
-  }
+class Humanoid extends CharacterStats{
+    constructor(attributes){
+    super(attributes);
+    this.team = attributes.team,
+    this.weapons = attributes.weapons,
+    this.language = attributes.language
+    }
+    greet(){
+        return `${this.name} offers a greeting in ${this.language}.`
+    }
+}
 
 const mage = new Humanoid({
     createdAt: new Date(),
